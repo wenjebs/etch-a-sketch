dd78bb8ccf4d65615dab7d3be8e0d6d33a04ae6d
wenjie <wenjieb2@gmail.com>
2022-07-19 22:23:23 +0800

    Put container creation in function and  create remove button

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 let size = 16;
+
 const row = document.querySelector(".row");
 const container = document.querySelector(".container");
-for (i = 0; i <= size-1; i++) {
-    let div = document.createElement("div");
-    row.appendChild(div);
-}
+function createContainer() {
 
-for (i = 0; i <= size-1; i++) {
-    let row2 = document.querySelector(".row");
-    container.appendChild(row2.cloneNode(true));
-}
+    let row = document.createElement("div");
+    row.classList.add('row');
+    container.appendChild(row);
 
-let boxNodeList = document.querySelectorAll(".row>div")
-
-boxNodeList.forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'yellow'
-    })
-
-});
-
-let resetBtn = document.querySelector(".resetButton")
-
-resetBtn.addEventListener('click', () => {
+    for (i = 0; i <= size-1; i++) {
+        let div = document.createElement("div");
+        row.appendChild(div);
+    }
+    
+    for (i = 0; i <= size-2; i++) {
+        let row = document.querySelector(".row");
+        container.appendChild(row.cloneNode(true));
+    }
+    
+    let boxNodeList = document.querySelectorAll(".row>div")
+    
     boxNodeList.forEach(box => {
-        box.style.backgroundColor = ''
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'yellow'
+        })
+    
+    });
+    
+    let resetBtn = document.querySelector(".resetButton")
+    
+    resetBtn.addEventListener('click', () => {
+        boxNodeList.forEach(box => {
+            box.style.backgroundColor = ''
+        })
     })
-})
+}
 
 let sizeBtn = document.querySelector(".sizeButton")
 
-sizeBtn.addEventListener('click', () => )
+sizeBtn.addEventListener('click', () => {
+    size = parseInt(prompt("Enter size of grid!"));
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    };
+    createContainer();
+})
+
+createContainer();
